4358c10c6fad64064e04ac66e458b6cfda79ba84
cbabroski <cbabroski@users.noreply.github.com>
2020-10-14 10:02:28 -0400

    nvim python setup

--- install.sh
+++ install.sh
@@ -2,7 +2,9 @@
 
 # Prerequisites
 brew install stow
-# TODO python3, ctags
+brew install python3
+python3 -m pip install --upgrade pip
+brew install git
 
 # Zsh setup with Oh My Zsh and Spaceship prompt
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
@@ -10,6 +12,10 @@ git clone https://github.com/denysdovhan/spaceship-prompt.git "$ZSH_CUSTOM/theme
 ln -s "$ZSH_CUSTOM/themes/spaceship-prompt/spaceship.zsh-theme" "$ZSH_CUSTOM/themes/spaceship.zsh-theme"
 rm ~/.zshrc # remove default zshrc
 stow zsh # symlink new zshrc to home directory
+
+# Universtal Ctags
+brew unlink ctags
+brew install --HEAD universal-ctags/universal-ctags/universal-ctags
 
 # FZF
 brew install fzf
@@ -19,11 +25,11 @@ $(brew --prefix)/opt/fzf/install
 brew install ripgrep
 
 # Neovim setup
-# TODO install neovim python dependencies
 brew install neovim
 mkdir -p ~/.config/nvim
+python3 -m pip install --user --upgrade pynvim
 cp nvim/init.vim ~/.config/nvim
-stow vim # symlink vimrc to home directory
+stow vim # symlink vimrc to home directory (which is used by nvim)
 
 # Apply changes
 source ~/.zshrc
